5143f25898dfd717e32e4bab28ad8e6ef5908602
TheodorBjorkman <70270703+TheodorBjorkman@users.noreply.github.com>
2021-05-10 22:35:01 +0200

    newnote

--- JS/script.js
+++ JS/script.js
@@ -3,5 +3,39 @@ function deleteNote() {
 }
 
 function createNote() {
-    //asdf
+    newNote = constructNote();
+    document.getElementById("plus").insertAdjacentElement("beforebegin", newNote);
+}
+
+function constructNote() {
+    const newNote = constructNoteBody();
+    const textbox = constructNoteTextbox();
+    const deleteButton = constructDeleteButton();
+    newNote.appendChild(textbox);
+    newNote.appendChild(deleteButton);
+    return newNote;
+}
+
+function constructNoteBody() {
+    const newNote = document.createElement("div");
+    const noteClasses = "box yellow";
+    newNote.classList = noteClasses;
+    return newNote;
+}
+
+function constructNoteTextbox() {
+    const textbox = document.createElement("textarea")
+    const textClasses = "box yellow";
+    textbox.classList = textClasses;
+    return textbox;
+}
+
+function constructDeleteButton() {
+    const deleteButton = document.createElement("button");
+    const delButtAttr = document.createAttribute("onclick");
+    const delButtClasses = "delete";
+    delButtAttr.value = "deleteNote()";
+    deleteButton.classList = delButtClasses;
+    deleteButton.setAttributeNode(delButtAttr);
+    return deleteButton;
 }
