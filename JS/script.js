20e22ffd41277a51105084d6b402e1c41351d2e4
TheodorBjorkman <70270703+TheodorBjorkman@users.noreply.github.com>
2021-05-14 01:50:20 +0200

    wörk

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,20 @@
-function deleteNote() {
-    document.activeElement.parentElement.remove();
+function deleteNote(id) {
+    const elements = document.getElementsByClassName(id);
+    for (let index = 0; index < elements.length; index++) {
+        const element = elements[index];
+        element.remove();
+    }
 }
 
 function createNote() {
     newNote = new Note;
-    document.getElementById("plus").insertAdjacentElement("beforebegin", newNote);
+    document.getElementById("empty").insertAdjacentElement("beforebegin", newNote);
 }
 
-function constructNoteBody(noteID) {
+function constructNoteBody(id) {
     const newNote = document.createElement("div");
-    const noteClasses = "box yellow fill";
+    const noteClasses = `box color note ${id}`;
     newNote.classList = noteClasses;
-    newNote.id = noteID;
     return newNote;
 }
 
@@ -24,33 +27,52 @@ function constructNoteTextbox() {
 
 function constructTag(tagType, tagClass, tagAttrType, tagAttrVal, inputType) {
     const tag = document.createElement(tagType);
-    const tagAttr = document.createAttribute(tagAttrType);
-    tagAttr.value = tagAttrVal;
     tag.classList = tagClass;
-    tag.setAttributeNode(tagAttr);
+    if (tagAttrType && tagAttrVal) {
+        const tagAttr = document.createAttribute(tagAttrType);
+        tagAttr.value = tagAttrVal;
+        tag.setAttributeNode(tagAttr);
+    }
+    if (inputType) {
+        const input = document.createAttribute("type");
+        input.value = inputType;
+        tag.setAttributeNode(input);
+    }
     return tag;
 }
 
-function formatUnorderedList() {
+function formatUnorderedList(id) {
 
 }
 
-function formatOrderedList() {
+function formatOrderedList(id) {
 
 }
 
-function formatNoList() {
+function formatNoList(id) {
+
+}
+
+function changeNoteColor(id) {
 
 }
 
 class Note {
     constructor() {
-        const newNote = constructNoteBody(Date.now());
+        const id = Date.now();
+        const newNote = constructNoteBody(id);
         const textbox = constructNoteTextbox();
         const tagCont = constructTag("div", "tags has-addons");
-        const delButt = constructTag("button", "tag delete is-medium is-danger", "onclick", "deleteNote()");
-        const colPick = constructTag("input")
-        tagCont.appendChild(tags)
+        const tags = [];
+        tags.push(constructTag("button", "tag delete is-medium is-danger", "onclick", `deleteNote(${id})`));
+        tags.push(constructTag("input", "tag", "onchange", `changeNoteColor(${id})`, "color"));
+        tags.push(constructTag("i", "fas fa-align-justify tag button", "onclick", `formatNoList(${id})`));
+        tags.push(constructTag("i", "fas fa-list tag button", "onclick", `formatUnorderedList(${id})`));
+        tags.push(constructTag("i", "fas fa-list-ol tag button", "onclick", `formatOrderedList(${id})`));
+        for (let index = 0; index < tags.length; index++) {
+            const curTag = tags[index];
+            tagCont.appendChild(curTag);
+        }
         newNote.appendChild(tagCont);
         newNote.appendChild(textbox);
         return newNote;
