38c1bc8b70f63136d3ca7024377719b94d946cc4
TheodorBjorkman <70270703+TheodorBjorkman@users.noreply.github.com>
2021-05-16 20:04:15 +0200

    wörk2

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,21 @@
+var notes = [];
+const LOCAL_STORAGE_KEY_NOTES = "app.notes";
+
+function start() {
+    notes = fetchLocal();
+    if (!notes) {
+        notes = [];
+        createNote();
+    }
+    loadStored();
+}
+
+function loadStored() {
+    while (notes) {
+        
+    }
+}
+
 function deleteNote(id) {
     const elements = document.getElementsByClassName(id);
     for (let index = 0; index < elements.length; index++) {
@@ -7,7 +25,8 @@ function deleteNote(id) {
 }
 
 function createNote() {
-    newNote = new Note;
+    newNote = note();
+    notes.push(newNote);
     document.getElementById("empty").insertAdjacentElement("beforebegin", newNote);
 }
 
@@ -18,14 +37,17 @@ function constructNoteBody(id) {
     return newNote;
 }
 
-function constructNoteTextbox() {
+function constructNoteTextbox(content) {
     const textbox = document.createElement("textarea")
     const textClasses = "box yellow";
     textbox.classList = textClasses;
+    if (content) {
+        textbox.innerHTML = content;
+    }
     return textbox;
 }
 
-function constructTag(tagType, tagClass, tagAttrType, tagAttrVal, inputType) {
+function constructTag(tagType, tagClass, tagAttrType, tagAttrVal, inputType, value) {
     const tag = document.createElement(tagType);
     tag.classList = tagClass;
     if (tagAttrType && tagAttrVal) {
@@ -37,6 +59,11 @@ function constructTag(tagType, tagClass, tagAttrType, tagAttrVal, inputType) {
         const input = document.createAttribute("type");
         input.value = inputType;
         tag.setAttributeNode(input);
+    }
+    if (value) {
+        const val = document.createAttribute("value");
+        val.value = value;
+        tag.setAttributeNode(val);
     }
     return tag;
 }
@@ -57,24 +84,29 @@ function changeNoteColor(id) {
 
 }
 
-class Note {
-    constructor() {
-        const id = Date.now();
-        const newNote = constructNoteBody(id);
-        const textbox = constructNoteTextbox();
-        const tagCont = constructTag("div", "tags has-addons");
-        const tags = [];
-        tags.push(constructTag("button", "tag delete is-medium is-danger", "onclick", `deleteNote(${id})`));
-        tags.push(constructTag("input", "tag", "onchange", `changeNoteColor(${id})`, "color"));
-        tags.push(constructTag("i", "fas fa-align-justify tag button", "onclick", `formatNoList(${id})`));
-        tags.push(constructTag("i", "fas fa-list tag button", "onclick", `formatUnorderedList(${id})`));
-        tags.push(constructTag("i", "fas fa-list-ol tag button", "onclick", `formatOrderedList(${id})`));
-        for (let index = 0; index < tags.length; index++) {
-            const curTag = tags[index];
-            tagCont.appendChild(curTag);
-        }
-        newNote.appendChild(tagCont);
-        newNote.appendChild(textbox);
-        return newNote;
+function fetchLocal() {
+    return JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_NOTES));
+}
+
+function storeLocal(list) {
+    localStorage.setItem(LOCAL_STORAGE_KEY_NOTES, JSON.stringify(list));
+}
+
+function note(id = Date.now(), color = "#ecec36", content) {
+    const newNote = constructNoteBody(id);
+    const textbox = constructNoteTextbox(content);
+    const tagCont = constructTag("div", "tags has-addons");
+    const tags = [];
+    tags.push(constructTag("button", "tag delete is-medium is-danger", "onclick", `deleteNote(${id})`));
+    tags.push(constructTag("input", "tag", "onchange", `changeNoteColor(${id})`, "color", `${color}`));
+    tags.push(constructTag("i", "fas fa-align-justify tag button", "onclick", `formatNoList(${id})`));
+    tags.push(constructTag("i", "fas fa-list tag button", "onclick", `formatUnorderedList(${id})`));
+    tags.push(constructTag("i", "fas fa-list-ol tag button", "onclick", `formatOrderedList(${id})`));
+    for (let index = 0; index < tags.length; index++) {
+        const curTag = tags[index];
+        tagCont.appendChild(curTag);
     }
+    newNote.appendChild(tagCont);
+    newNote.appendChild(textbox);
+    return newNote;
 }
